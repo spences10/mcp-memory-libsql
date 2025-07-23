9ed4378e7644b9cf173cfced253d15a9e28e4e8a
ZanzyTHEbar <zacariahheim@gmail.com>
2025-07-23 19:04:14 +0100

    feat: add jsonschema annotations to argument types for improved documentation

--- internal/apptype/mcp_types.go
+++ internal/apptype/mcp_types.go
@@ -2,39 +2,39 @@ package apptype
 
 // ProjectArgs provides a standard way to pass project context to tools.
 type ProjectArgs struct {
-	ProjectName string `json:"projectName,omitempty"`
+	ProjectName string `json:"projectName,omitempty" jsonschema:"The name of the project to operate on. If not provided, the default project is used."`
 }
 
 // CreateEntitiesArgs represents the arguments for the create_entities tool
 type CreateEntitiesArgs struct {
 	ProjectArgs
-	Entities []Entity `json:"entities"`
+	Entities []Entity `json:"entities" jsonschema:"A list of entities to create."`
 }
 
 // SearchNodesArgs represents the arguments for the search_nodes tool
 type SearchNodesArgs struct {
 	ProjectArgs
-	Query interface{} `json:"query"` // Can be string or []float32
+	Query interface{} `json:"query" jsonschema:"The search query. Can be a string for text search or a []float32 for vector similarity search."`
 }
 
 // CreateRelationsArgs represents the arguments for the create_relations tool
 type CreateRelationsArgs struct {
 	ProjectArgs
-	Relations []Relation `json:"relations"`
+	Relations []Relation `json:"relations" jsonschema:"A list of relations to create between entities."`
 }
 
 // DeleteEntityArgs represents the arguments for the delete_entity tool
 type DeleteEntityArgs struct {
 	ProjectArgs
-	Name string `json:"name"`
+	Name string `json:"name" jsonschema:"The name of the entity to delete."`
 }
 
 // DeleteRelationArgs represents the arguments for the delete_relation tool
 type DeleteRelationArgs struct {
 	ProjectArgs
-	Source string `json:"source"`
-	Target string `json:"target"`
-	Type   string `json:"type"`
+	Source string `json:"source" jsonschema:"The name of the source entity in the relation."`
+	Target string `json:"target" jsonschema:"The name of the target entity in the relation."`
+	Type   string `json:"type" jsonschema:"The type of the relation."`
 }
 
 // ReadGraphArgs represents the arguments for the read_graph tool
