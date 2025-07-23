06501a8b7dff28ee00bf9d4ffb3b7d926105000e
ZanzyTHEbar <zacariahheim@gmail.com>
2025-07-23 19:08:04 +0100

    refactor: standardize ProjectArgs field annotations across argument types

--- internal/apptype/mcp_types.go
+++ internal/apptype/mcp_types.go
@@ -7,39 +7,39 @@ type ProjectArgs struct {
 
 // CreateEntitiesArgs represents the arguments for the create_entities tool
 type CreateEntitiesArgs struct {
-	ProjectArgs
-	Entities []Entity `json:"entities" jsonschema:"A list of entities to create."`
+	ProjectArgs `json:"projectArgs,omitempty" jsonschema:"Project context for the operation."`
+	Entities    []Entity `json:"entities" jsonschema:"A list of entities to create."`
 }
 
 // SearchNodesArgs represents the arguments for the search_nodes tool
 type SearchNodesArgs struct {
-	ProjectArgs
-	Query interface{} `json:"query" jsonschema:"The search query. Can be a string for text search or a []float32 for vector similarity search."`
+	ProjectArgs `json:"projectArgs,omitempty" jsonschema:"Project context for the operation."`
+	Query       interface{} `json:"query" jsonschema:"The search query. Can be a string for text search or a []float32 for vector similarity search."`
 }
 
 // CreateRelationsArgs represents the arguments for the create_relations tool
 type CreateRelationsArgs struct {
-	ProjectArgs
-	Relations []Relation `json:"relations" jsonschema:"A list of relations to create between entities."`
+	ProjectArgs `json:"projectArgs,omitempty" jsonschema:"Project context for the operation."`
+	Relations   []Relation `json:"relations" jsonschema:"A list of relations to create between entities."`
 }
 
 // DeleteEntityArgs represents the arguments for the delete_entity tool
 type DeleteEntityArgs struct {
-	ProjectArgs
-	Name string `json:"name" jsonschema:"The name of the entity to delete."`
+	ProjectArgs `json:"projectArgs,omitempty" jsonschema:"Project context for the operation."`
+	Name        string `json:"name" jsonschema:"The name of the entity to delete."`
 }
 
 // DeleteRelationArgs represents the arguments for the delete_relation tool
 type DeleteRelationArgs struct {
-	ProjectArgs
-	Source string `json:"source" jsonschema:"The name of the source entity in the relation."`
-	Target string `json:"target" jsonschema:"The name of the target entity in the relation."`
-	Type   string `json:"type" jsonschema:"The type of the relation."`
+	ProjectArgs `json:"projectArgs,omitempty" jsonschema:"Project context for the operation."`
+	Source      string `json:"source" jsonschema:"The name of the source entity in the relation."`
+	Target      string `json:"target" jsonschema:"The name of the target entity in the relation."`
+	Type        string `json:"type" jsonschema:"The type of the relation."`
 }
 
 // ReadGraphArgs represents the arguments for the read_graph tool
 type ReadGraphArgs struct {
-	ProjectArgs
+	ProjectArgs `json:"projectArgs,omitempty" jsonschema:"Project context for the operation."`
 }
 
 // GraphResult represents the result for graph-related tools (search_nodes, read_graph)
